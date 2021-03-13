cf6666da685ac30f93004cfb369f254fa2d2ae3e
reyali <79721654+reyali@users.noreply.github.com>
2021-03-14 06:16:52 +0700

    Add files via upload

---
+++ plugins/tagall.js
@@ -0,0 +1,31 @@
+let { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text, participants }) => {
+     let [l, r] = text.split `|`
+         if (!l) return conn.reply(m.chat, 'Silahkan masukan nama anda\ncontoh: *#tagall Mas Adji|Sini Nimbrung*', m)
+    if (!r) return conn.reply(m.chat, 'Silahkan masukan pesan anda', m)
+
+  let users = participants.map(u => u.jid)
+  conn.reply(m.chat, '╭─ ➡️「 *TAG SEMUA MEMBER 」\n│\n├ ➡️ *Tukang Tag:* ' + l +  '\n├ ➡️ *Pesan:* ' + r +'\n│\n' + users.map(v => '├ 👉 @' + v.replace(/@.+/, '')).join`\n`, m, {
+    contextInfo: { mentionedJid: users }
+ 
+  
+})
+}
+handler.help = ['tagall <nama|pesan>']
+handler.tags = ['group']
+handler.command = /^tagall$/i
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.register = false
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
+
