722d42693cffa0feefa8a08635288515223f1730
reyali <79721654+reyali@users.noreply.github.com>
2021-03-14 09:05:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,37 @@
+global.owner = ['6282252420430'] // Put your number here
+global.mods = ['6282252420430'] // Want some help?
+global.prems = ['6282252420430'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+    nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  vhtear: 'https://api.vhtear.com',
+  mnazar: 'https://mnazria.herokuapp.com',
+  melodic: 'http://api-melodicxt-2.herokuapp.com',
+  vinz: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  vf: 'https://videfikri.com',
+  tobz: 'https://tobz-api.herokuapp.com'
+  
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.vhtear.com' : 'sensorom',
+  'http://api-melodicxt-2.herokuapp.com' : 'administrator',
+  'https://api.zeks.xyz' : 'apivinz',
+  'https://tobz-api.herokuapp.com' : 'BotWeA'
+}
+
+
+
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
