24eee5b1281be17948b60a8bb3a5aae941ed0340
reyali <79721654+reyali@users.noreply.github.com>
2021-03-15 00:50:00 +0700

    Add files via upload

---
+++ plugins/quran.js
@@ -0,0 +1,33 @@
+let fetch = require('node-fetch')
+let limit = 30
+let handler = async function (m, { text, isPrems, isOwner }) {
+	let user = global.DATABASE._data.users[m.sender]
+  if (user.registered === false) throw `Kamu belum terdaftar. Silahkan daftar Dulu\nKetik\n*#daftar Nama.Umur*\n*Contoh*\n#daftar Adji.19`
+  if (!text) throw 'masukkan nomor surah contoh #quran 2?'
+  let res = await fetch(global.API('vinz', '/api/quran', { no: text }, 'apikey'))
+  let { surah, asma, no, type, ket, audio, jumlah_ayat, ayat } = await res.json()
+  let surahq = `*${surah}*`
+						for (let i = 0; i < ayat.length; i++) {
+						 surahq +=  `\n➸ *Ayat*: ${ayat[i].number}\n➸ *Surah*: \n${ayat[i].text}\n➸ *Arti*: ${ayat[i].translation_id}\n\n=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=`
+			} 
+  await this.reply(m.chat, `=> Surah : ${surah}\n=> No : ${no}\n=> Asma : ${asma}\n=> Diturunkan di : ${type}\n=> Keterangan : ${ket}`, m)
+  await this.reply(m.chat, surahq,m)
+  await this.sendFile(m.chat, audio, 'audio.mp3',m)
+  
+
+}
+handler.help = ['quran <no surah>']
+handler.tags = ['new']
+handler.command = /^quran$/i
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+module.exports = handler  
