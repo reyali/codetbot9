6c91502d32de55ce35d947fed8d9901e65cbdf08
reyali <79721654+reyali@users.noreply.github.com>
2021-03-15 00:55:55 +0700

    Add files via upload

---
+++ plugins/tahlil.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+
+let handler = async(m, { conn, text }) => {
+
+    new Promise((reject) => {
+      
+
+        axios.get(`http://zekais-api.herokuapp.com/tahlil`)
+            .then((res) => {
+                // console.log(res.data.result)
+                
+                conn.reply(m.chat,`*arab*: ${res.data.arabic}\n*terjemah*: ${res.data.translate}`,m)
+              
+            })
+            conn.reply(m.chat,'sabar lg download',m)
+            .catch((err) => {
+                reject(err)
+            })
+    })
+}
+handler.help = ['tahlil']
+handler.tags = ['new']
+handler.command = /^tahlil$/i
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+module.exports = handler  
