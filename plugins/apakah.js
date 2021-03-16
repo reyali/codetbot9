36482692687261a5126504924bfe6ee69ff56709
reyali <79721654+reyali@users.noreply.github.com>
2021-03-16 13:16:37 +0700

    Add files via upload

---
+++ plugins/apakah.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, text }) => {
+  conn.reply(m.chat, `
+*Pertanyaan:* ${m.text}
+*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Bisa Jadi','Gatau Mls Jawab','Lu Mulu Yang Nanya Dri Tdi',' Emm entahlah','Tidak mungkin','Gua cape di tanya mulu'])}
+`.trim(), m)
+}
+handler.help = ['apakah <teks>?']
+handler.tags = ['kerang']
+handler.customPrefix = /(\?$)/
+handler.command = /^apakah$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
