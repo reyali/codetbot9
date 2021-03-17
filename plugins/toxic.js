3b5598127c5eedcf897a14fc1b279e406b20a16c
reyali <79721654+reyali@users.noreply.github.com>
2021-03-17 16:45:16 +0700

    Update toxic.js

--- plugins/toxic.js
+++ plugins/toxic.js
@@ -1,6 +1,6 @@
 const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^ontol|ntl|eler|elr|bi|abi|jg|nijing|njim|jinc|eki|emek|mk|ngsat|gst|ngsad|gsd|ncok|tod|td|ingan|gn|blok|lk|ego|egok$/i, '')
+  let count = command.replace(/^ontol|ntl|eler|elr|bi|abi|jg|njing|njim|jinc|eki|emek|mk|ngsat|gst|ngsad|gsd|ncok|tod|td|ingan|gn|blok|lk|ego|egok$/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.DATABASE._data.users[m.sender].exp >= xpperlimit * count) {
@@ -11,7 +11,7 @@ let handler = async (m, { conn, command, args }) => {
 }
 handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
 handler.tags = ['xp']
-handler.command = /^ontol|ntl|eler|elr|bi|abi|jg|nijing|njim|jinc|eki|emek|mk|ngsat|gst|ngsad|gsd|ncok|tod|td|ingan|gn|blok|lk|ego|egok$/i
+handler.command = /^ontol|ntl|eler|elr|bi|abi|jg|njing|njim|jinc|eki|emek|mk|ngsat|gst|ngsad|gsd|ncok|tod|td|ingan|gn|blok|lk|ego|egok$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
