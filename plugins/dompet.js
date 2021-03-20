f53ad487446c3c9553e6f44ba6961a4e536beee1
reyali <79721654+reyali@users.noreply.github.com>
2021-03-20 13:37:09 +0700

    Add files via upload

---
+++ plugins/dompet.js
@@ -0,0 +1,41 @@
+let handler = async (m, { text }) => {
+	let user = global.DATABASE._data.users[m.sender]
+	let { name, limit, exp, lastclaim, registered, regTime, age } = global.DATABASE.data.users[m.sender]
+	  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
+	let usersExp = sortedExp.map(v => v[0])
+	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  if (user.registered === false) throw `Lu blm daftar bego ketik #daftar dulu`
+count = Math.floor(Math.random() * 20000) +1
+  buah = ['✅','✅',] // Versi Simpel
+          satu = buah[Math.floor(Math.random() * (buah.length))]	
+          dua = buah[Math.floor(Math.random() * (buah.length))]	
+          tiga = buah[Math.floor(Math.random() * (buah.length))]	
+if (satu === dua && dua === tiga)
+	await m.reply(`╭─ ✅ *Dompet ${nam
+┃
+├─ ➡️ Nama: *${name}*
+├─ ➡️ Exp  : *${exp}* Exp
+├─ ➡️ Coin: *${limit}* Coin
+┗━ 🏆 Leaderboard/level: *Kamu berada diposisi ${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
+
+Note: Perhitungan nya adalah *100* Exp = *1* Coin
+
+`)
+	
+}
+handler.help = ['dompet']
+handler.tags = ['xp']
+handler.command = /^(dompet|limit|exp)$/i
+
+handler.owner = false 
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+module.exports = handler  
